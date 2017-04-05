f27d63ba7771a3c92b60074a8ebf26a80122398f
Sofia Nguy <sofianguy@gmail.com>
2017-04-05 15:31:39 -0700

    test image-to-ascii module

--- hello.js
+++ hello.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
 console.log(`HELLO ${process.argv[2]}`)
+
+// Dependencies
+const imageToAscii = require("image-to-ascii");
+
+// The path can be either a local path or an url
+imageToAscii("https://octodex.github.com/images/octofez.png", (err, converted) => {
+    console.log(err || converted);
+});
+
+// Passing options
+imageToAscii("https://octodex.github.com/images/privateinvestocat.jpg", {
+    colored: false
+}, (err, converted) => {
+    console.log(err || converted);
+});
